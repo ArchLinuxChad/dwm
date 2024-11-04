47eab87a662e31f4ce8ebb92260d227851b1ddf4
ArchLinuxChad <thedillonking@proton.me>
2024-11-04 21:31:22 +0000

    updated

--- config.h
+++ config.h
@@ -35,6 +35,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gedit",    NULL,       NULL,       1 << 3,            0,           -1},
 };
 
 /* layout(s) */
@@ -64,7 +65,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
